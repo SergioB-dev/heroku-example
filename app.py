56be130bddf030695d73f57dc6712f5fd4f2e536
Sergio <bostshakur@gmail.com>
2022-06-25 15:33:08 -0700

    add port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def get_cat():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
